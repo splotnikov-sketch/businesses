655d333e0629283e90123105c39c6bbadccec51c
Sergey Plotnikov <Sergey.Plotnikov@perficient.com>
2022-11-24 11:51:59 -0500

    removed ts version of api to cleanup repo

--- businesses-api-ts/bin/utils/express.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.writeJsonResponse = void 0;
-function writeJsonResponse(res, code, payload, headers) {
-    const data = typeof payload === 'object' ? JSON.stringify(payload, null, 2) : payload;
-    res.writeHead(code, Object.assign(Object.assign({}, headers), { 'Content-Type': 'application/json' }));
-    res.end(data);
-}
-exports.writeJsonResponse = writeJsonResponse;
